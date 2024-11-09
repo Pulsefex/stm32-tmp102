a00044de8bb0ff380fdf0c458de36ee8157842d8
leroymusa <musaleroyohio@gmail.com>
2024-11-09 18:24:18 -0500

    add remaining macros

--- tmp102.h
+++ tmp102.h
@@ -9,16 +9,22 @@
 #ifndef TMP102_H
 #define TMP102_H
 
+/*********i2c & Reg Defs************/
+#define TMP102_I2C_ADDR     (0x48<<1)   //Shift left for HAL
+#define TMP102_TEMP_REG     0x00        //Temp Reg
+#define TMP102_CONFIG_REG   0x01        //Config Reg
 
-#define TMP102_I2C_ADDR     0x48
-#define TMP102_RESOLUTION   0.0625
+/*********Error Codes************/
+#define TMP102_ERR_OK       0           //No error
+#define TMP102_ERR_WRITE    1           //Write error
+#define TMP102_ERR_READ     2           //Read error
+
+/*********Config Options************/
+#define TMP102_SD_MODE      (1<<8)      //Shutdown mode
 
 
-
-
-
-
-
+/*********Conversion Constant****/
+#define TMP102_RESOLUTION   0.0625      //Resolution in Celsius per LSB
 
 
 
